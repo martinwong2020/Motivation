3c3166203b24cf529fd2b921a4cd57751766b953
Martin Wong <martinytwong2020@gmail.com>
2022-09-12 15:41:35 -0700

    welcome msg for entry pg

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
             </li>
           </ul>
         </div>
-        <About/>
+        <h1>Welcome to the Page</h1>
         <Routes>
           <Route exact path="/about" element={<About/>}></Route>
           <Route exact path="/gumball" element={<Gumball/>}></Route>
